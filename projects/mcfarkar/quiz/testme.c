1e945fd7501ee75e16e4f9b934fabb37e953d54e
mcfarkar <mcfarkar@oregonstate.edu>
2019-02-10 11:59:07 -0800

    Random Testing Quiz submission

---
+++ projects/mcfarkar/quiz/testme.c
@@ -0,0 +1,82 @@
+
+/*  
+ ** Name: Karen McFarland 
+ ** Date: 2/10/2019 
+ ** Random Testing (RT) Quiz 
+**  File:testme.c
+*/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // TODO: rewrite this function
+    // this function should output random valid input
+	// Valid input: printable ASCII characters whose code is between 32-126
+
+    return 32 + rand() % 95;
+}
+
+char *inputString()
+{
+    // TODO: rewrite this function
+    // this function should output a random 5 character string
+	// Valid input: printable ASCII characters whose code is between 97-122
+	static char rdmStr[5];
+
+	// returns "reset" approximately every 100 calls
+	int probSpace = 1 + rand() % 100; 
+	if(probSpace < 99){
+		for(int i = 0; i < 5; i++){
+			rdmStr[i] = 97 + rand() % 26; 
+		}
+	}
+	else
+		strcpy(rdmStr, "reset");
+
+    return rdmStr;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
